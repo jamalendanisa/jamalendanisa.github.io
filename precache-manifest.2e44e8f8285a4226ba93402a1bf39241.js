9e9317b6a71c6db8385ce8d9bf7ee5bd845885b4
jamalendanisa <anisa.jamalenda@gmail.com>
2023-03-07 23:36:30 +0700

    Updates

---
+++ precache-manifest.2e44e8f8285a4226ba93402a1bf39241.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d7826cb9b1b6747a7d705e5f73a920",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e532f1b5362e9aa16ff",
+    "url": "/static/css/2.6406e80c.chunk.css"
+  },
+  {
+    "revision": "68d6416c6ebdc676326f",
+    "url": "/static/css/main.df22fb38.chunk.css"
+  },
+  {
+    "revision": "7e532f1b5362e9aa16ff",
+    "url": "/static/js/2.3503e2db.chunk.js"
+  },
+  {
+    "revision": "ce3126c332c3bc8f1544c88c174f7c51",
+    "url": "/static/js/2.3503e2db.chunk.js.LICENSE"
+  },
+  {
+    "revision": "68d6416c6ebdc676326f",
+    "url": "/static/js/main.cf3dd807.chunk.js"
+  },
+  {
+    "revision": "691ca8fda51f2fe961ac",
+    "url": "/static/js/runtime-main.0d6ca171.js"
+  }
+]);
